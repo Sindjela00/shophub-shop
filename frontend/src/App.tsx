4b2ad02d64234684437783f9e1fd3652b8f0cd90
Sindjela00 <aco.sindjelic@gmail.com>
2026-08-18 17:08:36 +0200

    Revert "fix: make the bundled frontend work when served behind a path prefix"
    This reverts commit 9acf7b605e6397c2cb02a6a88f72c8051985b084.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { ToastProvider } from '@/context/toast-context'
 import { Header } from '@/components/layout/header'
 import { Footer } from '@/components/layout/footer'
 import { AdminLayout } from '@/components/layout/admin-layout'
-import { basePath } from '@/lib/base-path'
 import { CatalogPage } from '@/pages/catalog-page'
 import { ProductPage } from '@/pages/product-page'
 import { CartPage } from '@/pages/cart-page'
@@ -28,7 +27,7 @@ function StorefrontLayout() {
 
 function App() {
   return (
-    <BrowserRouter basename={basePath}>
+    <BrowserRouter>
       <ToastProvider>
         <CartProvider>
           <Routes>
